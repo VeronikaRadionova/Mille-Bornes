37dc1c223fc72babd2c8142d4e575bffc1c77ff8
VeronikaRadionova <114427643+VeronikaRadionova@users.noreply.github.com>
2024-03-08 12:01:26 +0100

    TP2 presque fin

--- MilleBornes/src/testsFonctionnels/TestTP2.java
+++ MilleBornes/src/testsFonctionnels/TestTP2.java
@@ -1,5 +1,7 @@
 package testsFonctionnels;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import cartes.Attaque;
@@ -11,6 +13,7 @@ import cartes.FinLimite;
 import cartes.JeuDeCartes;
 import cartes.Parade;
 import cartes.Probleme.Type;
+import utils.Utils;
 
 public class TestTP2 {
 
@@ -48,8 +51,44 @@ public class TestTP2 {
 		}
 		
 		// checkCount
-		System.out.println("\nVérification des quantités d'exeplaires pour toutes les cartes: " + cartes.checkCount() + "\n");
+		System.out.println("\nVérification des quantités d'exeplaires pour toutes les cartes: " + cartes.checkCount() + "\n\n");
 		
+		
+		System.out.println("TEST des méthodes 'melanger', 'rassembler' et 'verifierRassemblement':\n");
+		
+		JeuDeCartes jeu = new JeuDeCartes();
+		List<Carte> listeCarteNonMelangee = jeu.getListeCartes();
+		List<Carte> listeCartes = new ArrayList<>(listeCarteNonMelangee); System.out.println(listeCartes);
+		listeCartes = Utils.melanger(listeCartes);
+		System.out.println(listeCartes);
+		System.out.println("liste mélangée sans erreur ? " + Utils.verifierMelange(listeCarteNonMelangee, listeCartes));
+		listeCartes = Utils.rassembler(listeCartes);
+		System.out.println(listeCartes + "\n");
+		
+		// sur des tableaux proposés
+		Integer[] tableau0 = {};
+        List<Integer> liste0 = Arrays.asList(tableau0);
+        System.out.println("[]: " /*+ Utils.verifierRassemblement(liste0)*/);
+        liste0 = Utils.rassembler(liste0);
+        System.out.println(liste0.toString() + ": " /*+ Utils.verifierRassemblement(liste0)*/ + "\n");
+        
+		Integer[] tableau1 = {1, 1, 2, 1, 3};
+        List<Integer> liste1 = Arrays.asList(tableau1);
+        System.out.println("[1, 1, 2, 1, 3]: " /*+ Utils.verifierRassemblement(liste1)*/);
+        liste1 = Utils.rassembler(liste1);
+        System.out.println(liste1.toString() + ": " + /*Utils.verifierRassemblement(liste1) +*/ "\n");
+		
+        Integer[] tableau2 = {1, 4, 3, 2};
+        List<Integer> liste2 = Arrays.asList(tableau2);
+        System.out.println("[1, 4, 3, 2]: " /*+ Utils.verifierRassemblement(liste2)*/);
+        liste2 = Utils.rassembler(liste2);
+        System.out.println(liste2.toString() + ": " + /*Utils.verifierRassemblement(liste2) +*/ "\n");
+        
+        Integer[] tableau3 = {1, 1, 2, 3, 1};
+        List<Integer> liste3 = Arrays.asList(tableau3);
+        System.out.println("[1, 1, 2, 3, 1]: " /*+ Utils.verifierRassemblement(liste3)*/);
+        liste3 = Utils.rassembler(liste3);
+        System.out.println(liste3.toString() + ": " + /*Utils.verifierRassemblement(liste3) +*/ "\n");
 	}
 
 }
--- MilleBornes/src/utils/Utils.java
+++ MilleBornes/src/utils/Utils.java
@@ -1,8 +1,79 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Random;
 
 public class Utils {
 	private static Random rnd = new Random();
-
+	
+	public static <T> T extraire (List <T> liste) {
+		int rndIndex = rnd.nextInt(liste.size());
+		return liste.remove(rndIndex);
+	}
+	
+	public static <T> T extraireIterateur (List <T> liste) {
+		int rndIndex = rnd.nextInt(liste.size());
+		ListIterator <T> it = liste.listIterator(rndIndex);
+		T elem = it.next();
+		it.remove();
+		return elem;
+	}
+	
+	public static <T> List <T> melanger (List <T> liste) {
+		List <T> listeMelange = new LinkedList<>();
+		while (!liste.isEmpty()) {
+			T elem = extraire(liste);
+			listeMelange.add(elem);
+		}
+		return listeMelange;
+	}
+	
+	public static <T> boolean verifierMelange (List <T> liste1, List <T> liste2) {
+		for (T elem: liste1) {
+			if (Collections.frequency(liste1, elem) != Collections.frequency(liste2, elem)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static <T> List <T> rassembler (List <T> liste) {
+		List <T> listeCons = new ArrayList<>();
+		
+		if (liste.isEmpty() || liste == null) {
+			return listeCons;
+		}
+		
+		for (T elem: liste) {
+			if (!listeCons.contains(elem)) {
+				for (int j = 0; j < Collections.frequency(liste, elem); j++) {
+					listeCons.add(elem);
+				}
+			}
+		}
+		
+		return listeCons;
+	}
+	
+	public static <T> boolean verifierRassemblement (List <T> liste) {
+		ListIterator <T> it1 = liste.listIterator();
+		ListIterator <T> it2 = liste.listIterator();
+		
+		
+		
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
